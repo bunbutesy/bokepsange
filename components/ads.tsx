ae4d464e7711bfc6ed9ccb3dab857fcc6e89056c
bunbutesy <164426281+bunbutesy@users.noreply.github.com>
2024-04-27 22:10:41 +0700

    Update ads.tsx

--- components/ads.tsx
+++ components/ads.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 
 const AdScript = () => (
- <Script src="\/\/februaryinjectionproudly.com\/e4\/bd\/fc\/e4bdfc68a5382d728c8f05f785b5447b.js.js"/>
+ <Script src="\/\/februaryinjectionproudly.com\/e4\/bd\/fc\/e4bdfc68a5382d728c8f05f785b5447b.js"/>
 );
 
 export default AdScript;
